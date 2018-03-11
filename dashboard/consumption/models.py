ac99cce4bd0efb9e20cf6214d389097a9436ae8d
femi <olufemidokun@hotmail.co.uk>
2018-03-11 09:54:52 +0000

    models created

--- dashboard/consumption/models.py
+++ dashboard/consumption/models.py
@@ -4,3 +4,13 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    UserId = models.IntegerField()
+    area = models.CharField(max_length=3)
+    tariff = models.CharField(max_length=3)
+
+class Consumption(models.Model):
+    date_time = models.DateTimeField()
+    user_consumption = models.FloatField()
+
